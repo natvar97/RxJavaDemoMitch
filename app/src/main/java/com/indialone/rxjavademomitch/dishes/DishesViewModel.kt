73059119896b5aa710ea48c95de7b88458ed6835
Natavar Prajapati <prajapatinatavar21197@gmail.com>
2021-07-28 18:57:50 +0530

    dish details updated with mvvm and using of fromCallable function to get dishes details

--- app/src/main/java/com/indialone/rxjavademomitch/dishes/DishesViewModel.kt
+++ app/src/main/java/com/indialone/rxjavademomitch/dishes/DishesViewModel.kt
@@ -9,7 +9,9 @@ import com.indialone.rxjavademomitch.dishes.models.search.SearchResponse
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.Disposables
 import io.reactivex.schedulers.Schedulers
 import okhttp3.ResponseBody
 
@@ -19,63 +21,98 @@ class DishesViewModel : ViewModel() {
 
     private val dishes = MutableLiveData<SearchResponse>()
     private val dishDetails = MutableLiveData<RecipeDetailsResponse>()
+    private val compositeDisposable = CompositeDisposable()
+    private lateinit var disposables: Disposables
 
     init {
         fetchDishes("")
         fetchDishDetails("")
     }
 
-    fun fetchDishDetails(recipeId: String) {
-        dishesRepository
-            .getDishDetails(recipeId)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : Observer<RecipeDetailsResponse> {
-                override fun onSubscribe(d: Disposable) {
+    fun fetchDishDetails(recipeId: String) =
+        Observable.fromCallable {
+            dishesRepository.getDishDetails(recipeId)
+        }.subscribe {
+            it.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<RecipeDetailsResponse> {
+                    override fun onSubscribe(d: Disposable) {
 
-                }
+                    }
 
-                override fun onNext(t: RecipeDetailsResponse) {
-                    dishDetails.value = t
-                }
+                    override fun onNext(t: RecipeDetailsResponse) {
+                        dishDetails.value = t
+                    }
 
-                override fun onError(e: Throwable) {
+                    override fun onError(e: Throwable) {
 
-                }
+                    }
 
-                override fun onComplete() {
+                    override fun onComplete() {
 
-                }
+                    }
 
-            })
-    }
+                })
 
-    fun fetchDishes(selection: String) {
-        dishesRepository.getRecipes(selection)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : Observer<SearchResponse> {
-                override fun onSubscribe(d: Disposable) {
 
-                }
+//        dishesRepository
+//            .getDishDetails(recipeId)
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe(object : Observer<RecipeDetailsResponse> {
+//                override fun onSubscribe(d: Disposable) {
+//
+//                }
+//
+//                override fun onNext(t: RecipeDetailsResponse) {
+//                    dishDetails.value = t
+//                }
+//
+//                override fun onError(e: Throwable) {
+//
+//                }
+//
+//                override fun onComplete() {
+//
+//                }
+//
+//            })
+        }
 
-                override fun onNext(t: SearchResponse) {
-                    dishes.value = t
-                }
+    fun fetchDishes(selection: String) = Observable
+        .fromCallable {
+            dishesRepository.getRecipes(selection)
+        }
+        .subscribe {
+            it.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<SearchResponse> {
+                    override fun onSubscribe(d: Disposable) {
 
-                override fun onError(e: Throwable) {
+                    }
 
-                }
+                    override fun onNext(t: SearchResponse) {
+                        dishes.value = t
+                    }
 
-                override fun onComplete() {
+                    override fun onError(e: Throwable) {
 
-                }
+                    }
 
-            })
-    }
+                    override fun onComplete() {
+
+                    }
+
+                })
+        }
+
 
     fun getDishes(): LiveData<SearchResponse> = dishes
 
     fun getDishDetails(): LiveData<RecipeDetailsResponse> = dishDetails
 
+    override fun onCleared() {
+        super.onCleared()
+
+    }
 }
